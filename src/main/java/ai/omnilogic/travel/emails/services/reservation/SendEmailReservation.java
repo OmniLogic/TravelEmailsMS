6d9b9db75ff2717e1e9be8777feb1a8e8c277d0f
luanp <luanpereiranc@gmail.com>
2022-10-20 07:59:51 -0300

    Adjusted name application

--- src/main/java/ai/omnilogic/travel/emails/services/reservation/SendEmailReservation.java
+++
@@ -1,8 +0,0 @@
-package ai.omnilogic.travel.emails.services.reservation;
-
-public interface SendEmailReservation {
-
-    void sendRequesteReservation(OrderReserve orderReserve);
-
-    void reSendConfirmReserveMail(OrderReserve orderReserve, String login);
-}
